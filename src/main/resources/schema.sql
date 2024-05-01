93e0b6cda5a456751eb125ee8c866706d67de5f2
BurningFalls <sherlocky31415@gmail.com>
2024-05-01 09:48:19 +0900

    database: reservation_time에 already_booked attribute 추가

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -4,8 +4,9 @@ DROP TABLE IF EXISTS theme;
 
 CREATE TABLE reservation_time
 (
-    id       BIGINT       NOT NULL AUTO_INCREMENT,
-    start_at VARCHAR(255) NOT NULL,
+    id             BIGINT       NOT NULL AUTO_INCREMENT,
+    start_at       VARCHAR(255) NOT NULL,
+    already_booked BOOLEAN      NOT NULL DEFAULT FALSE,
     PRIMARY KEY (id)
 );
 
