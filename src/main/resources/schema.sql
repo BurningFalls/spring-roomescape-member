9b39529e983a5d040a0655621a47f3450ece2571
BurningFalls <sherlocky31415@gmail.com>
2024-05-01 16:41:40 +0900

    database: 인기 테마 조회 API 테스트를 위해 샘플 데이터 수정

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -31,17 +31,11 @@ CREATE TABLE theme
 );
 
 INSERT INTO theme (name, description, thumbnail)
-VALUES ('정글 모험', '열대 정글의 심연을 탐험하세요.', 'https://example.com/thumbnails/jungle.jpg'),
-       ('우주 오디세이', '별들을 넘어 우주 여행을 떠나세요.', 'https://example.com/thumbnails/space.jpg'),
-       ('보물 찾기', '잃어버린 보물을 찾아 모험을 떠나세요.', 'https://example.com/thumbnails/treasure.jpg'),
-       ('미스터리 저택', '저택 안에 숨겨진 미스터리를 풀어보세요.', 'https://example.com/thumbnails/mystery.jpg'),
-       ('해적의 만', '바다를 항해하며 묻힌 금을 찾아보세요.', 'https://example.com/thumbnails/pirate.jpg'),
-       ('유령의 할로윈', '유령의 집에서 스릴과 오싹함을 경험하세요.', 'https://example.com/thumbnails/halloween.jpg'),
-       ('겨울 왕국', '눈 덮인 풍경의 마법과 아름다움을 즐기세요.', 'https://example.com/thumbnails/winter.jpg'),
-       ('슈퍼히어로 사가', '슈퍼히어로가 되어 도시를 악에서 구하세요.', 'https://example.com/thumbnails/superhero.jpg'),
-       ('역사적 탐험', '역사적 장소를 탐험하며 과거로 여행하세요.', 'https://example.com/thumbnails/historical.jpg'),
-       ('사막 탈출', '뜨거운 사막을 지나 탈출을 시도하세요.', 'https://example.com/thumbnails/desert.jpg');
-
+VALUES ('정글 모험', '열대 정글의 심연을 탐험하세요.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('우주 오디세이', '별들을 넘어 우주 여행을 떠나세요.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('보물 찾기', '잃어버린 보물을 찾아 모험을 떠나세요.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('미스터리 저택', '저택 안에 숨겨진 미스터리를 풀어보세요.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg'),
+       ('해적의 만', '바다를 항해하며 묻힌 금을 찾아보세요.', 'https://i.pinimg.com/236x/6e/bc/46/6ebc461a94a49f9ea3b8bbe2204145d4.jpg');
 
 CREATE TABLE reservation
 (
@@ -54,3 +48,12 @@ CREATE TABLE reservation
     FOREIGN KEY (time_id) REFERENCES reservation_time (id),
     FOREIGN KEY (theme_id) REFERENCES theme (id)
 );
+
+INSERT INTO reservation (name, date, time_id, theme_id)
+VALUES ('John Doe', '2024-04-23', 1, 1),
+       ('Jane Smith', '2024-04-24', 2, 2),
+       ('Alice Johnson', '2024-04-25', 3, 1),
+       ('Bob Brown', '2024-04-26', 4, 3),
+       ('Charlie Davis', '2024-04-27', 5, 1),
+       ('Diana Adams', '2024-04-28', 6, 2),
+       ('Evan Wright', '2024-04-29', 7, 2);
